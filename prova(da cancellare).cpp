475e47a8865d9addb18e8c0d3d60af014ccd3df1
PrXdGy <lorenzocirilli8@gmail.com>
2018-05-07 20:24:14 +0200

    Add files via upload

---
+++ prova(da cancellare).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float pepsi;
+cout << "Inserisci il prio numero\n";
+cin >> pepsi;
+if (pepsi<=10)
+{
+	cout << "Beh, mi dispiace ma ti attacchi al cazzo!!\n";
+}
+else 
+{
+	cout << "bravo, bella scelta!!\n";
+}
+return 0;
+}
+
+
+
