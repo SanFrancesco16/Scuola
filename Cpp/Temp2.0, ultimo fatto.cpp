a684cda7cb43a44ddf0ca38c612b4f8762a78b0c
PrXdGy <lorenzocirilli8@gmail.com>
2019-02-03 13:36:44 +0100

    Update and rename Cpp/Temp2.0.cpp to  Cpp/Temp2.0, ultimo fatto.cpp

--- Cpp/Temp2.0, ultimo fatto.cpp
+++ Cpp/Temp2.0, ultimo fatto.cpp
@@ -33,7 +33,7 @@ int main(){
 	int presente=0;
 	
 	do{
-		cout<<"PROGRAMMA TEMPERATURE\n\n0.esci\n1.inserimento\n2.ricerce temp piu' basse\n3.Salvataggio su file temp min e temp max\n4.calcola escusione termica per ogni giorno\n5.Salvataggio su file completo\n6.ricerca temp piu' alte\n\nFai una scelta: ";
+		cout<<"PROGRAMMA TEMPERATURE\n\n0.esci\n1.inserimento\n2.ricerce temp piu' basse\n3.Salvataggio su file temp min e temp max\n4.calcola escusione termica per ogni giorno\n5.Salvataggio su file completo\n6.ricerca temp piu' alte\n7.calcola media giornaliera\n8.calcola media settmanale\n\nFai una scelta: ";
 		cin>>scelta;
 		switch(scelta){
 			case 1:
@@ -53,11 +53,11 @@ int main(){
 				cin>>tempinser;
 				for(int i=0;i<G;i++){
 					if(sett[i].getmin()<tempinser){
-						cout<<"temperatura: "<<sett[i].getmin()<<"il giorno: "<<i+1<<endl;
+						cout<<"temperatura: "<<sett[i].getmin()<<"	il giorno: "<<i+1<<endl;
 						presente++;
 					}
 					if(sett[i].getmax()<tempinser){
-						cout<<"temperatura: "<<sett[i].getmax()<<"il giorno: "<<i+1<<endl;
+						cout<<"temperatura: "<<sett[i].getmax()<<"	il giorno: "<<i+1<<endl;
 						presente++;
 					}
 				}
@@ -66,7 +66,7 @@ int main(){
 				}		
 			break;
 			case 3:
-				f1.open("C:\\Users\\Roberto\\Desktop\\Temperature_settimanali.txt",ios::out);
+				f1.open("C:\\Users\\loren\\Desktop\\Temperature_settimanali.txt",ios::out);
 				for(int i=0;i<G;i++){
 					f1<<"giorno: "<<i+1<<"      temp min: "<<sett[i].getmin()<<"      temp max: "<<sett[i].getmax()<<endl;
 				}
@@ -79,11 +79,19 @@ int main(){
 				}
 			break;
 			case 5:
-				f1.open("C:\\Users\\Roberto\\Desktop\\Temperature_settimanali.txt",ios::out);
+				float m_s;//media_settimanale
+				float a;//appo
+				float mg[G];//media_giorno[G]
 				for(int i=0;i<G;i++){
-					f1<<"giorno: "<<i+1<<"      temp min: "<<sett[i].getmin()<<"      temp max: "<<sett[i].getmax()<<"      escursione termica: "<<sett[i].getdiff()<<endl;
+					mg[i]=(sett[i].getmax()+sett[i].getmin())/2;
+					a=a+mg[i];
 				}
-				
+				m_s=a/G;
+				f1.open("C:\\Users\\loren\\Desktop\\Temperature_settimanali.txt",ios::out);
+				for(int i=0;i<G;i++){
+					f1<<"giorno: "<<i+1<<"      temp min: "<<sett[i].getmin()<<"      temp max: "<<sett[i].getmax()<<"      escursione termica: "<<sett[i].getdiff()<<"		media del giorno: "<<mg[i]<<endl;
+				}
+				f1<<"\n\nMEDIA SETTIMANALE: "<<m_s<<endl;
 				f1.close();
 				cout<<"File savato"<<endl;
 			break;
@@ -93,12 +101,30 @@ int main(){
 				cin>>temp_per_max;
 				for(int i=0;i<G;i++){
 					if(temp_per_max<sett[i].getmax()){
-						cout<<"temperatura: "<<sett[i].getmax()<<"il giorno: "<<i+1<<endl;
+						cout<<"temperatura: "<<sett[i].getmax()<<"	il giorno: "<<i+1<<endl;
 					}
 					if(temp_per_max<sett[i].getmin()){
-						cout<<"temperatura: "<<sett[i].getmin()<<"il giorno: "<<i+1<<endl;
+						cout<<"temperatura: "<<sett[i].getmin()<<"	il giorno: "<<i+1<<endl;
 					}
 				}
+			break;
+			case 7:
+				float media_giornaliera[G];
+				for(int i=0;i<G;i++){
+					media_giornaliera[i]=(sett[i].getmax()+sett[i].getmin())/2;
+					cout<<"temp media: "<<media_giornaliera[i]<<"	giorno: "<<i+1<<endl;
+				}
+			break;
+			case 8:
+				float media_settimanale;
+				float appo;
+				float media_giorno[G];
+				for(int i=0;i<G;i++){
+					media_giorno[i]=(sett[i].getmax()+sett[i].getmin())/2;
+					appo=appo+media_giorno[i];
+				}
+				media_settimanale=appo/G;
+				cout<<"media settimanale: "<<media_settimanale<<endl;
 			break;
 		}
 	}while(scelta!=0);
